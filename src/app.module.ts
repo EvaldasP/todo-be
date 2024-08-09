c09e3aa8eef3ca58b0f331ad88d83f88cba74f1e
Evaldas Pocius <evaldukas96@gmail.com>
2024-08-09 18:47:15 +0300

    initialize db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { Todo } from './todo/entities/todo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'todo-database.sqlite',
+      entities: [User, Todo],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
