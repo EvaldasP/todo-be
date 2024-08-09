1fa3aa3febbdeb269fa78e54ce6e98aaae111afc
Evaldas Pocius <evaldukas96@gmail.com>
2024-08-09 08:58:39 +0300

    create entities

---
+++ src/todo/entities/todo.entity.ts
@@ -0,0 +1,20 @@
+import { User } from 'src/user/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: false })
+  completed: boolean;
+
+  @ManyToOne(() => User, (user) => user.todos)
+  user: User;
+}
