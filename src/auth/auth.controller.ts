297cb4e0d8a588f9f6367027baa11c878d2bdc2a
Evaldas Pocius <evaldukas96@gmail.com>
2024-09-21 16:10:03 +0300

    functionality to create new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ClassSerializerInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { UserService } from 'src/user/services/user.service';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly _authService: AuthService) {}
+  constructor(
+    private readonly _authService: AuthService,
+    private readonly _userService: UserService,
+  ) {}
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto): Promise<{
+    access_token: string;
+  }> {
     return this._authService.login(loginDto);
+  }
+
+  @Post('register')
+  async register(@Body() registerDto: any): Promise<string> {
+    return this._userService.createUser(registerDto);
   }
 }
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,5 +13,25 @@ export class UserService {
 
   async findOneByUsername(username: string): Promise<User> {
     return this._userRepository.findOne({ where: { username } });
+  }
+
+  async createUser(createUserDto: any): Promise<string> {
+    const { username, password } = createUserDto;
+
+    const user = await this.findOneByUsername(username);
+
+    if (user) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = this._userRepository.create({
+      username: username,
+      password: hashedPassword,
+    });
+
+    await this._userRepository.save(newUser);
+    return newUser.username;
   }
 }
